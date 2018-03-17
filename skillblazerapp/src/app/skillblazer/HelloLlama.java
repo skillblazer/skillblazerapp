6033d473de68433d8501a7883e9d7ea787ce4f26
rebec <rebec@REBECCA-LAPTOP>
2018-03-17 16:28:10 -0400

    Added print statements.

--- skillblazerapp/src/app/skillblazer/HelloLlama.java
+++ skillblazerapp/src/app/skillblazer/HelloLlama.java
@@ -1,8 +1,10 @@
 package app.skillblazer;
 
 public class HelloLlama {
-	
-	public static void main(String[] args) {
-		System.out.println("Hello, Llamas!!");
-	}
+
+    public static void main(String[] args) {
+        System.out.println("Hello, Llamas!!");
+        System.out.println("I love llamas"); // added by R.Davis
+        System.out.println("#MultiTabLife"); // added by R.Davis
+    }
 }
